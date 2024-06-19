386c3e25405ea45638fce4d127c388d720eacf5f
saikatmitra91 <10416686+saikatmitra91@users.noreply.github.com>
2024-06-19 11:49:31 +0000

    [create-pull-request] automated change

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -16,3 +16,10 @@ test("open home page and login components should be visible", async ({
   ).toBeVisible();
   await page.close();
 });
+
+test("check for email and password elements", async ({ page }) => {
+  await page.goto("https://test-generator-dashboard.vercel.app/");
+  await expect(page.locator('input[type="email"]')).toBeVisible();
+  await expect(page.locator('input[type="password"]')).toBeVisible();
+  await page.close();
+});
