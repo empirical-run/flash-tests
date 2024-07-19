b8beccd8faf892d3f7181c2735d4cfc01dfef8eb
Saikat Mitra <saikatmitra91@gmail.com>
2024-07-19 14:16:09 +0530

    [create-pull-request] automated change (#102)
    Co-authored-by: arushsharma24 <46960231+arushsharma24@users.noreply.github.com>

---
+++ tests/preview.spec.ts
@@ -0,0 +1,11 @@
+import { test } from "@playwright/test";
+
+test("test group preview caching", async ({ page }) => {
+  //
+  // login to flash with arush@empirical.run and password QncNV6!u!FTcTBF
+  // let the dashboard open
+  // click on the add button
+  // type the group preview name "preview"
+  // test the the group preview is upto date
+  //
+});
