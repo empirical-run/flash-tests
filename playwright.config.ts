bde40ea582d00299d6e090811648cb508eab282e
Arjun Attam <arjunattam@gmail.com>
2024-07-25 15:24:49 +0530

    feat: enable cross browser tests (#109)
    * feat: enable cross browser tests
    
    * fix install:browsers script
    
    * ci: switch to macos
    
    * use ref
    
    * trigger
    
    * trigger
    
    * trigger
    
    * change quotes
    
    * escape
    
    * change args
    
    * trigger
    
    * change runner to warp-macos-latest-arm64-6x
    
    * undo ref

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,5 +8,13 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "safari",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
