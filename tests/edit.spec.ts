b3f5fea5cd98df12d6216e584e6e86a941d3f126
Saikat Mitra <saikatmitra91@gmail.com>
2024-07-29 13:44:14 +0530

    test: test case session should be visible for already added test (#133)
    * [create-pull-request] automated change
    
    * Automated commit by GitHub Action
    
    ---------
    
    Co-authored-by: saikatmitra91 <10416686+saikatmitra91@users.noreply.github.com>

---
+++ tests/edit.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("test case session should be visible for already added test", async ({
+  page,
+}) => {
+  await page.goto("https://test-generator-dashboard.vercel.app/");
+  await page.fill('input[type="email"]', "automation-test@empirical.run");
+  await page.fill('input[type="password"]', "xiYk85Mw.mZNLfg");
+  await page.click('button[type="submit"]');
+  await expect(page.getByRole("button", { name: "Add" })).toBeVisible();
+
+  await page
+    .getByRole("link", { name: "Import new wallet using keplr" })
+    .click();
+  await expect(
+    page.getByRole("heading", { name: "Test case session" }),
+  ).toBeVisible();
+  await expect(page.getByRole("button", { name: "Send" })).toBeVisible();
+});
