c722c59ac150b3d8fbedac9103c3720e6cf67d52
Saikat  Mitra <saikatmitra91@gmail.com>
2024-09-18 16:04:34 +0530

    fix: tests

--- tests/edit.spec.ts
+++ tests/edit.spec.ts
@@ -8,11 +8,9 @@ test("test case session should be visible for already added test", async ({
   await page.fill('input[type="password"]', "xiYk85Mw.mZNLfg");
   await page.click('button[type="submit"]');
   await expect(page.getByRole("button", { name: "Add" })).toBeVisible();
-
   await page
     .getByRole("link", { name: "Import new wallet using keplr" })
     .click();
-  await page.reload();
   await expect(page.getByText("Import new wallet using keplr")).toBeVisible();
   await expect(page.getByRole("button", { name: "Send" })).toBeVisible();
 });
