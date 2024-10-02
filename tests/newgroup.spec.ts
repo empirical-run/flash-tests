a394f1f505ef80639e768808477cf4a9a5df5fcc
Saikat Mitra <saikatmitra91@gmail.com>
2024-10-02 16:21:13 +0530

    revert: unintended changes (#268)

--- tests/newgroup.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "./fixtures";
-
-test("test multipart", async ({ page }) => {
-  await page.goto("https://www.google.com");
-
-  await page.getByLabel("Search", { exact: true }).fill("airbnb");
-});
