f99b78f4de2e01663a729fddf5734153c6016334
Saikat Mitra <saikatmitra91@gmail.com>
2024-11-26 00:29:09 +0530

    feat: disable web security (#436)

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,16 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          args: [
+            "--disable-web-security",
+            "--disable-site-isolation-trials", // Required to bypass strict site isolation
+            "--disable-features=IsolateOrigins,site-per-process", // Complements the above
+          ],
+        },
+      },
     },
     {
       name: "firefox",
