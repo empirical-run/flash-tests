2ef9a4d00d8dec2ee4c0139262c888b8f3864722
Pushpam Saxena <43639045+pushpam5@users.noreply.github.com>
2024-12-27 20:04:53 +0530

    test: serial tests added (#834)

---
+++ tests/serial-test.spec.ts
@@ -0,0 +1,20 @@
+import { test } from "./fixtures";
+
+test.describe.serial("Serial test group", () => {
+  test("Flaky test", async ({ page, userContext }) => {
+    const randomValue = Math.random();
+    console.log(`Random value: ${randomValue}`);
+
+    await page.fill('input[type="email"]', userContext.email);
+    await page.fill('input[type="password"]', userContext.password);
+
+    if (randomValue > 0.4) {
+      throw new Error("Flaky error triggered because random value > 0.4");
+    }
+  });
+
+  test("Dependent test", async ({ page, userContext }) => {
+    await page.fill('input[type="email"]', userContext.email);
+    await page.fill('input[type="password"]', userContext.password);
+  });
+});
