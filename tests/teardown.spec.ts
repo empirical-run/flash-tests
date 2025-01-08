4b2401cdc9523fd23775f6896582cb8a4c6b871a
Saikat Mitra <saikatmitra91@gmail.com>
2025-01-08 16:37:41 +0530

    fix: teardown script (#986)

--- tests/teardown.spec.ts
+++ tests/teardown.spec.ts
@@ -4,13 +4,11 @@ test("Teardown: Close all automation test sessions", async ({
   loggedInPage,
 }) => {
   await loggedInPage.getByRole("link", { name: "Sessions" }).click();
-
-  while (true) {
-    const sessionRow = await loggedInPage
-      .getByRole("row", { name: /^automation-test@empirical.run/ })
-      .first();
-    if (!(await sessionRow.isVisible())) break;
-
+  await loggedInPage.waitForTimeout(5_000);
+  const sessionRow = loggedInPage
+    .getByRole("row", { name: /^automation-test@empirical.run/ })
+    .first();
+  while (await sessionRow.isVisible()) {
     await sessionRow.getByRole("button").click();
     await loggedInPage.getByRole("button", { name: "Close session" }).click();
     await loggedInPage.waitForLoadState("networkidle");
